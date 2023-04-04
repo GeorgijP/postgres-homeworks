e583ea7ee3274a7e160403ba1994267542117dd0
GeorgijP <georgij.puzanov@inbox.ru>
2023-04-04 19:29:31 +0400

    Записал команды создания таблиц

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	first_name varchar(30),
+	last_name varchar(30),
+	title varchar(50),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(5) PRIMARY KEY,
+	company_name varchar(30),
+	contact_name varchar(30)
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(5),
+	employee_id int,
+	order_date date,
+	ship_city varchar(30)
+);
